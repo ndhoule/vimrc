4c85034b1863fe6b6689b9d5dfacd1fc6175fe40
Nathan Houle <nathan@nathanhoule.com>
2014-06-21 21:44:05 -0700

    Bootstrap Vundle during setup

--- install.rb
+++ install.rb
@@ -1,8 +1,19 @@
 #!/usr/bin/env ruby
 
 require 'etc'
+require 'FileUtils'
 
+VUNDLE_URL = 'git://github.com/gmarik/Vundle.vim.git'
+VUNDLE_PATH = File.expand_path(File.join(File.dirname(__FILE__), 'bundle/vundle'))
 HOME = Etc.getpwuid.dir
+
+# TODO: npm install tern, jsctags, jshint, jsbeautify
+
+# If Vundle hasn't been bootstrapped yet, clone it into place
+if not File.directory?(VUNDLE_PATH)
+  FileUtils.mkdir_p(File.dirname(VUNDLE_PATH))
+  system("git clone #{VUNDLE_URL} #{VUNDLE_PATH}")
+end
 
 # Write a shim ~.vimrc file to load the file located at ~/.vim/vimrc
 # TODO: Back up existing vimrc file
