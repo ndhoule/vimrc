ndhoule/vimrc

commit 8e2333be9d4bc209bd02d6f7b9dc559c00f953f6

Add LanguageClient for autocompletion, definition jump, etc.
For details and documentation, see:

- https://microsoft.github.io/language-server-protocol/
- http://langserver.org/
- https://github.com/Microsoft/language-server-protocol
- https://github.com/autozimu/LanguageClient-neovim

after/plugin/investigate.vim
@@ -8,6 +8,4 @@ let g:investigate_use_dash_for_javascript=1
 " Keybindings
 ""
 
-" Override default man search command with investigate hotkeys
-nnoremap <silent> K :call investigate#Investigate('n')<CR>
-vnoremap <silent> K :call investigate#Investigate('v')<CR>
+nnoremap <silent> <F1> :call :call investigate#Investigate('n')<CR>

after/plugin/languageclient.vim
@@ -1,0 +1,27 @@
+" Required for operations modifying multiple buffers like rename.
+set hidden
+
+" Start language servers automatically
+let g:LanguageClient_autoStart = 1
+
+let g:LanguageClient_serverCommands = {
+    \ 'css': ['css-languageserver', '--stdio'],
+    \ 'dockerfile': ['docker-langserver', '--stdio'],
+    \ 'javascript': ['flow-language-server', '--stdio'],
+    \ 'javascript.jsx': ['flow-language-server', '--stdio', '--try-flow-bin'],
+    \ 'json': ['json-languageserver', '--stdio'],
+    \ 'less': ['css-languageserver', '--stdio'],
+    \ 'sass': ['css-languageserver', '--stdio'],
+    \ 'typescript': ['javascript-typescript-stdio'],
+    \ }
+
+" https://github.com/autozimu/LanguageClient-neovim/blob/next/doc/LanguageClient.txt
+" TODO(ndhoule): Revisit these shortcuts
+" TODO(ndhoule): Add mode-specific mappings, or at least warn when a language
+"                server doesn't implement a call
+nnoremap <silent> K :call LanguageClient_textDocument_hover()<CR>
+nnoremap <silent> gd :call LanguageClient_textDocument_definition()<CR>
+nnoremap <silent> gr :call LanguageClient_textDocument_references()<CR>
+nnoremap <silent> <F10> :call LanguageClient_textDocument_documentSymbol()<CR>
+nnoremap <silent> <F11> :call LanguageClient_workspace_symbol()<CR>
+nnoremap <silent> <F12> :call LanguageClient_textDocument_rename()<CR>

init.vim
@@ -165,13 +165,13 @@ map <Leader>p "+p
 cmap w!! w !sudo tee % >/dev/null
 
 " Yank the current filename to the OS clipboard
-noremap <silent> <F1> :let @+=expand("%:p")<CR>
+noremap <silent> <F2> :let @+=expand("%:p")<CR>
 
 " Toggle between regular numbering, relative numbering, no numbering
-nmap <silent> <F2> :exec &nu==&rnu ? 'se nu!' : 'se rnu!'<CR>
-
-" Enable paste mode--helps quash indent, etc. problems when pasting blocks of code
-nnoremap <F3> :set invpaste paste?<CR>
+nmap <silent> <F3> :exec &nu==&rnu ? 'se nu!' : 'se rnu!'<CR>
 
 " Toggle spell check
 nnoremap <F4> :set spell!<CR>
+
+" Enable paste mode--helps quash indent, etc. problems when pasting blocks of code
+nnoremap <F5> :set invpaste paste?<CR>

plugins.vim
@@ -35,6 +35,7 @@ call plug#begin(s:plugins_path)
 " - Locate fzf dynamically for use on e.g. Linux?
 
 " Utilities
+
 Plug '/usr/local/opt/fzf' | Plug 'junegunn/fzf.vim'
 Plug 'EinfachToll/DidYouMean'
 Plug 'Raimondi/delimitMate'
@@ -65,14 +66,39 @@ Plug 'vim-airline/vim-airline-themes'
 Plug 'vim-scripts/Rename'
 Plug 'w0rp/ale'
 
-" Language
-Plug 'heavenshell/vim-jsdoc'
+" Language Server Protocol
+" https://microsoft.github.io/language-server-protocol/
+" http://langserver.org/
+
+" TODO(ndhoule): Should run :UpdateRemotePlugins
+Plug 'autozimu/LanguageClient-neovim', { 'branch': 'next', 'do': './install.sh' }
+
+" TODO(ndhoule): Install language clients locally?
+"
+" - https://github.com/sourcegraph/javascript-typescript-langserver
+" - https://github.com/flowtype/flow-language-server
+" - https://github.com/rcjsuen/dockerfile-language-server-nodejs
+" - https://github.com/vscode-langservers/vscode-css-languageserver-bin
+" - https://github.com/vscode-langservers/vscode-json-languageserver-bin
+"
+" - yarn global add \
+"   javascript-typescript-langserver \
+"   flow-language-server \
+"   dockerfile-language-server-nodejs \
+"   vscode-json-languageserver-bin
+"
+" TODO(ndhoule)): Try out:
+" - https://github.com/vscode-langservers/vscode-html-languageserver-bin
+"
+
+" Filetypes
+
 Plug 'sheerun/vim-polyglot', { 'do:': 'gometalinter --install --update' }
 
 " Completion
+
 Plug 'Shougo/deoplete.nvim', { 'do': ':UpdateRemotePlugins' }
 Plug 'Shougo/neosnippet.vim'
-Plug 'wokalski/autocomplete-flow'
 
 " OS-Specific
 if g:host_os == 'Darwin'

commit 2d8410343e302f20eb52feb849f5148747f908fb

Fix lulzy error introduced in 8e2333be9d4bc209bd02d6f7b9dc559c00f953f6

after/plugin/investigate.vim
@@ -8,4 +8,5 @@ let g:investigate_use_dash_for_javascript=1
 " Keybindings
 ""
 
-nnoremap <silent> <F1> :call :call investigate#Investigate('n')<CR>
+nnoremap <silent> <F1> :call investigate#Investigate('n')<CR>
+vnoremap <silent> <F1> :call investigate#Investigate('v')<CR>