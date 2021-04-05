99b8077c7fac8cebc66a320565199b060972a857
Nathan Houle <nathan@nathanhoule.com>
2021-04-05 14:55:12 -0700

    chore: update plugins, limit plugin update concurrency

--- lua/ndhoule_plugins.lua
+++ lua/ndhoule_plugins.lua
@@ -1,185 +1,190 @@
 vim.cmd [[packadd packer.nvim]]
 
-return require('packer').startup(function()
-  use {'wbthomason/packer.nvim', opt = true}
+return require("packer").startup({
+  function()
+    use {'wbthomason/packer.nvim', opt = true}
 
-  -- Add functions for finding the current project's root directory path
-  use {'dbakker/vim-projectroot', commit = '68787126ebbbe50e120e497ff63696b22f636840'}
+    -- Add functions for finding the current project's root directory path
+    use {'dbakker/vim-projectroot', commit = '68787126ebbbe50e120e497ff63696b22f636840'}
 
-  -- ## Core behavior
+    -- ## Core behavior
 
-  -- Use a common set of vim defaults
-  use 'tpope/vim-sensible'
+    -- Use a common set of vim defaults
+    use 'tpope/vim-sensible'
 
-  -- Add a killring
-  use {'bfredl/nvim-miniyank', commit = '2a3a0f3ae535e1b93a8c17dfdac718b9a12c772b'}
+    -- Add a killring
+    use {'bfredl/nvim-miniyank', commit = '2a3a0f3ae535e1b93a8c17dfdac718b9a12c772b'}
 
-  -- Highlight trailing whitespace, with support for blacklisting filetypes
-  use {'ntpeters/vim-better-whitespace', commit = '8cf4b2175dd61416c2fe7d3234324a6c59d678de'}
+    -- Highlight trailing whitespace, with support for blacklisting filetypes
+    use {'ntpeters/vim-better-whitespace', commit = '8cf4b2175dd61416c2fe7d3234324a6c59d678de'}
 
-  -- Automatically close word control structures (if/fi, do/end, etc.)
-  use {'tpope/vim-endwise', commit = '97180a73ad26e1dcc1eebe8de201f7189eb08344'}
+    -- Automatically close word control structures (if/fi, do/end, etc.)
+    use {'tpope/vim-endwise', commit = '97180a73ad26e1dcc1eebe8de201f7189eb08344'}
 
-  -- Add support for `.` (repeat) for plugins
-  use {'tpope/vim-repeat', commit = 'c947ad2b6a16983724a0153bdf7f66d7a80a32ca'}
+    -- Add support for `.` (repeat) for plugins
+    use {'tpope/vim-repeat', commit = '24afe922e6a05891756ecf331f39a1f6743d3d5a'}
 
-  -- ## New core commands
+    -- ## New core commands
 
-  -- Suggest alternatives when opening a misspelled file (e.g. 'index.j' -> 'index.js')
-  use {'EinfachToll/DidYouMean', commit = 'a98fb17ba16aa2eb69b3f9cc7a70e8a736890e4e'}
+    -- Suggest alternatives when opening a misspelled file (e.g. 'index.j' -> 'index.js')
+    use {'EinfachToll/DidYouMean', commit = 'a98fb17ba16aa2eb69b3f9cc7a70e8a736890e4e'}
 
-  -- Open a scratch buffer (clone of emacs functionality)
-  use {'mtth/scratch.vim', commit = '6df617ebc0695bd9839a4fe365a08df13d24bc05'}
+    -- Open a scratch buffer (clone of emacs functionality)
+    use {'mtth/scratch.vim', commit = 'b3f643bfdefb83b47f0ecc03d815f4a564c5e687'}
 
-  -- Rename the current buffer on the filesystem
-  use {'vim-scripts/Rename', tag = '0.3'}
+    -- Rename the current buffer on the filesystem
+    use {'vim-scripts/Rename', tag = '0.3'}
 
-  -- ## Search
+    -- ## Search
 
-  -- Make * and # work on visual selections
-  use {'bronson/vim-visual-star-search', commit = 'e48c3fe596229e38f5a0e5313455e835c14aeb6a'}
+    -- Make * and # work on visual selections
+    use {'bronson/vim-visual-star-search', commit = 'e48c3fe596230e38f5a0e5313455e835c14aeb6a'}
 
-  -- ## Project navigation
+    -- ## Project navigation
 
-  -- Fuzzy file finder and file contents searcher
-  use {
-    'junegunn/fzf.vim',
-    commit = 'e9d62b4c873f5f207202b4ba5bbd63de7003a0d3',
-    requires = {
-      {
-        'junegunn/fzf',
-        commit = 'ab3937ee5a62d63bac9307bfe72601eeb4fc9cd2',
-        run = './install --bin'
+    -- Fuzzy file finder and file contents searcher
+    use {
+      'junegunn/fzf.vim',
+      commit = '707f5b6269337ea5c19fc4054cd2574939f817f5',
+      requires = {
+        {
+          'junegunn/fzf',
+          commit = '7acdaf0b4335a6c089bce5b73ff23aeb3b9d9c92',
+          run = './install --bin'
+        }
       }
     }
-  }
 
-  -- Menu for navigating ctags, LSP symbols, etc.
-  use {'liuchengxu/vista.vim', commit = 'd77828b043d980b99e386840d57629f6499e9995'}
+    -- Menu for navigating ctags, LSP symbols, etc.
+    use {'liuchengxu/vista.vim', commit = '4387164845165634a06941b17c2b4f398cffd193'}
 
-  -- File navigation menu
-  use {'preservim/nerdtree', commit = '14af89743ac1c31ff9bb43682025eda50333a7d5'}
+    -- File navigation menu
+    use {'preservim/nerdtree', commit = '1b19089917cc3e0a81d3294fead2424c419d545c'}
 
-  -- ## Statusline
+    -- ## Statusline
 
-  -- Customize statusline
-  use {'itchyny/lightline.vim', commit = '709b2d8dc88fa622d6c076f34b05b58fcccf393f'}
+    -- Customize statusline
+    use {'itchyny/lightline.vim', commit = '8e013f32f524157bf14ccaa87d97be3d3a7201e2'}
 
-  -- ## Motions
+    -- ## Motions
 
-  -- Add motions for sorting within a line or range of lines
-  use {'christoomey/vim-sort-motion', commit = '49dfcabeee2bf3a85a6cc0774b35f687b6c9d0e5'}
+    -- Add motions for sorting within a line or range of lines
+    use {'christoomey/vim-sort-motion', commit = '49dfcabeee2bf3a85a6cc0774b35f687b6c9d0e5'}
 
-  -- Add motions for expanding a visual cursor within a group
-  use {'gcmt/wildfire.vim', commit = 'e2baded7162260e05d2527f5bca9fca81f0bc8f2'}
+    -- Add motions for expanding a visual cursor within a group
+    use {'gcmt/wildfire.vim', commit = 'e2baded7162260e05d2527f5bca9fca81f0bc8f2'}
 
-  -- Add motions for commenting out blocks of code
-  use {'tomtom/tcomment_vim', commit = '2037d05870e10e03b2e157a09ae007c179efd253'}
+    -- Add motions for commenting out blocks of code
+    use {'b3nj5m1n/kommentary', commit = '5adfea5864b11f8d43b07162b0d80c6d64d0d056'}
 
-  -- Add mappings for working on text within pairs of characters (e.g. quotes)
-  use {'tpope/vim-surround', commit = 'f51a26d3710629d031806305b6c8727189cd1935'}
+    -- Add mappings for working on text within pairs of characters (e.g. quotes)
+    use {'machakann/vim-sandwich', commit = '9e6340affe9f53c11a6975a5f50b9bf48adb692c'}
 
-  -- ## Text editing
+    -- ## Text editing
 
-  -- Extras for Markdown editing, including TOC generation
-  use {'SidOfc/mkdx', commit = '43f602ee353e46171e846a3010f0125b12eab6b4'}
+    -- Extras for Markdown editing, including TOC generation
+    use {'SidOfc/mkdx', commit = '602a78430aee47881f8c57f73ba96fdded9a3ace'}
 
-  -- ## Job execution
+    -- ## Job execution
 
-  -- Run jobs asynchronously in the background
-  use {'tpope/vim-dispatch', commit = 'fe6a34322829e466a7e8ce710a6ac5eabddff9fd'}
+    -- Run jobs asynchronously in the background
+    use {'tpope/vim-dispatch', commit = 'fe6a34322829e466a7e8ce710a6ac5eabddff9fd'}
 
-  -- Integrate with linters, fixers, formatters, etc.
-  use {'dense-analysis/ale', commit = 'f996ede5999c99b1b3e3cecc02dbd06cb286d3ff'}
+    -- Integrate with linters, fixers, formatters, etc.
+    use {'dense-analysis/ale', commit = '388cf3374312b05122151bc68691bf09a69ff840'}
 
-  -- Debugging
-  use {
-    'puremourning/vimspector',
-    commit = '78bec87f4ed058dfad6f8c33f487719db10f1bd3',
-    run = ':VimspectorUpdate'
-  }
+    -- Debugging
+    use {
+      'puremourning/vimspector',
+      commit = '78bec87f4ed058dfad6f8c33f487719db10f1bd3',
+      run = ':VimspectorUpdate'
+    }
 
-  -- ## VCS
+    -- ## VCS
 
-  use {'mhinz/vim-signify', commit = '98c693f7a1a91b73d9232d868765b4d20af892fe'}
+    use {'mhinz/vim-signify', commit = 'b2a0450e23c63b75bbeabf4f0c28f9b4b2480689'}
 
-  -- Add vim commands for Git operations
-  use {'tpope/vim-fugitive', commit = '660d2ba2b60026a9069ee005562ddad6a67f9fb0'}
+    -- Add vim commands for Git operations
+    use {'tpope/vim-fugitive', commit = '8cf0cf5bfb2b858faecf4e0f6c1b8d0948805e5e'}
 
-  -- Add vim-fugitive GitHub integration
-  use {'tpope/vim-rhubarb', commit = 'c509c7eedeea641f5b0bdae708581ff610fbff5b'}
+    -- Add vim-fugitive GitHub integration
+    use {'tpope/vim-rhubarb', commit = 'd865e427d067af57d85cf2b7d2bc1912eb84d0bf'}
 
-  -- ## Environment
+    -- ## Environment
 
-  -- FIXME(ndhoule): direnv + autochdir throws errors when changing directory via fzf on neovim 0.5+.
-  -- if executable('direnv')
-  --   -- Source direnv files, keeping vim environment/subshells in sync with host shell env
-  --   use {'direnv/direnv.vim', commit = 'ff37d76da391e1ef299d2f5eb84006cb27a67799'}
-  -- endif
+    -- FIXME(ndhoule): direnv + autochdir throws errors when changing directory via fzf on neovim 0.5+.
+    -- if executable('direnv')
+    --   -- Source direnv files, keeping vim environment/subshells in sync with host shell env
+    --   use {'direnv/direnv.vim', commit = 'ff37d76da391e1ef299d2f5eb84006cb27a67799'}
+    -- endif
 
-  -- ## Project settings
+    -- ## Project settings
 
-  -- Add integration for .editorconfig files
-  use {'editorconfig/editorconfig-vim', commit = '047c4b455b9d56adee63a52d6d7cdf5bb6521d53'}
+    -- Add integration for .editorconfig files
+    use {'editorconfig/editorconfig-vim', commit = '047c4b455b9d56adee63a52d6d7cdf5bb6521d53'}
 
-  -- Add support for project configuration files ('projections')
-  use {'tpope/vim-projectionist', commit = '0c6967d3a8c3bd3d3b42013531c3f12c843aff80'}
+    -- Add support for project configuration files ('projections')
+    use {'tpope/vim-projectionist', commit = '348e070867d02bd471df486bfbe25e2e2ce13061'}
 
-  -- Load project-specific vimrc (.lvimrc) files
-  use {'embear/vim-localvimrc', commit = 'ac6444afb5fd11e3f7750f696a0c6b8b0b6ec116'}
+    -- Load project-specific vimrc (.lvimrc) files
+    use {'embear/vim-localvimrc', commit = 'ac6444afb5fd11e3f7750f696a0c6b8b0b6ec116'}
 
-  -- ## Text editing
+    -- ## Text editing
 
-  -- Insert matching pairs of characters in insert mode (e.g. (), [], '')
-  use {'Raimondi/delimitMate', commit = '537a1da0fa5eeb88640425c37e545af933c56e1b'}
+    -- Insert matching pairs of characters in insert mode (e.g. (), [], '')
+    use {'Raimondi/delimitMate', commit = '537a1da0fa5eeb88640425c37e545af933c56e1b'}
 
-  -- ## Language Server Protocol
+    -- ## Language Server Protocol
 
-  -- - http://langserver.org/
-  -- - https://microsoft.github.io/language-server-protocol/
-  -- - :help lsp
-  use {
-    'neovim/nvim-lspconfig',
-    commit = 'dbc4bf514198f9e328e200a9aa63b462d6e1e9f1',
-    disable = not vim.g.ENABLE_LSP
-  }
+    -- - http://langserver.org/
+    -- - https://microsoft.github.io/language-server-protocol/
+    -- - :help lsp
+    use {
+      'neovim/nvim-lspconfig',
+      commit = '041dfd7fb648d24c80cb8829dda2469f66f88490',
+      disable = not vim.g.ENABLE_LSP
+    }
 
-  -- ## Syntax highlighting
+    -- ## Syntax highlighting
 
-  -- Hightlight pairs of characters with different colors
-  use {'luochen1990/rainbow', commit = '4d15633cdaf61602e1d9fd216a77fc02e0881b2d'}
+    -- Hightlight pairs of characters with different colors
+    use {'luochen1990/rainbow', commit = '4d15633cdaf61602e1d9fd216a77fc02e0881b2d'}
 
-  -- Change background color to highlight indentation levels
-  use {'nathanaelkane/vim-indent-guides', commit = '54d889a63716ee2f1818aa2ec5082db47147147b'}
+    -- Change background color to highlight indentation levels
+    use {'nathanaelkane/vim-indent-guides', commit = '54d889a63716ee2f1818aa2ec5082db47147147b'}
 
-  -- Highlight color names and codes in the same color that they represent
-  use {'chrisbra/Colorizer', commit = '879e6c69c0c02c4ef0f08b3955c60de16efd8fb8'}
+    -- Highlight color names and codes in the same color that they represent
+    use {'chrisbra/Colorizer', commit = '826d5691ac7d36589591314621047b1b9d89ed34'}
 
-  use {
-    'nvim-treesitter/nvim-treesitter',
-    disable = not vim.g.ENABLE_TREESITTER,
-    run = ':TSUpdate'
-  }
-  use {'sheerun/vim-polyglot', tag = 'v4.16.0'}
+    use {
+      'nvim-treesitter/nvim-treesitter',
+      disable = not vim.g.ENABLE_TREESITTER,
+      run = ':TSUpdate'
+    }
+    use {'sheerun/vim-polyglot', tag = 'v4.17.0'}
 
-  -- ## Completion
+    -- ## Completion
 
-  use {
-    'nvim-lua/completion-nvim',
-    commit = '936bbd17577101a4ffb07ea7f860f77dd8007d43'
-  }
-  use {
-    'steelsojka/completion-buffers',
-    commit = '441a58b77c04409e8ccb35fd4970598ae551462f',
-    disable = not vim.g.ENABLE_COMPLETION_NVIM
-  }
-  use {
-    'nvim-treesitter/completion-treesitter',
-    commit = '45c9b2faff4785539a0d0c655440c2465fed985a',
-    disable = not vim.g.ENABLE_TREESITTER and not vim.g.ENABLE_COMPLETION_NVIM
-  }
+    use {
+      'nvim-lua/completion-nvim',
+      commit = 'fc9b2fd2d47bea6a8954de1b1b19f2330545b354'
+    }
+    use {
+      'steelsojka/completion-buffers',
+      commit = 'c36871b2a44b59761387f4972c617b44dcec5e75',
+      disable = not vim.g.ENABLE_COMPLETION_NVIM
+    }
+    use {
+      'nvim-treesitter/completion-treesitter',
+      commit = '45c9b2faff4785539a0d0c655440c2465fed985a',
+      disable = not vim.g.ENABLE_TREESITTER and not vim.g.ENABLE_COMPLETION_NVIM
+    }
 
-  -- ## Theming
+    -- ## Theming
 
-  use {'sainnhe/sonokai', commit = '51e7ee8b13f9c2e4eae33f8d745eaa1f320b0ae6'}
-end)
+    use {'sainnhe/sonokai', commit = '80523e137cd6df42c2875db3db92576f00c91377'}
+  end,
+  config = {
+    max_jobs = 10,
+  },
+});
