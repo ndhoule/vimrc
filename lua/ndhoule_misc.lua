9061d9e2e64d1496d52fcb63bcb356f6a3b7d8f7
Nathan Houle <nathan@nathanhoule.com>
2021-04-05 15:04:33 -0700

    feat: comment motions use single-line comments by default
    Unlike vim-commentary, kommentary uses multi-line comment syntax
    whenever it can. I prefer single-line comments as I often uncomment
    sections within a commented block, so this commit makes single-line
    comments the default behavior.

--- lua/ndhoule_misc.lua
+++ lua/ndhoule_misc.lua
@@ -7,3 +7,7 @@ vim.cmd('nnoremap <silent> <Leader>S :Scratch<CR>')
 
 -- Set up surround.vim-compatible keybindings for vim-sandwich
 vim.cmd('runtime macros/sandwich/keymap/surround.vim')
+
+require('kommentary.config').configure_language("default", {
+  prefer_single_line_comments = true,
+})
