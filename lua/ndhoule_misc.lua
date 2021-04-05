2fd7f952c6ad22ea857539953e2a4f49d283ba58
Nathan Houle <nathan@nathanhoule.com>
2021-04-05 14:56:16 -0700

    feat: use vim-surround keybindings in vim-sandwich

--- lua/ndhoule_misc.lua
+++ lua/ndhoule_misc.lua
@@ -4,3 +4,6 @@
 
 -- Open a scratch buffer
 vim.cmd('nnoremap <silent> <Leader>S :Scratch<CR>')
+
+-- Set up surround.vim-compatible keybindings for vim-sandwich
+vim.cmd('runtime macros/sandwich/keymap/surround.vim')
