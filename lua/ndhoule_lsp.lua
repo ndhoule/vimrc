20a075914e72d54173b08327dded13efc125f2c9
Nathan Houle <nathan@nathanhoule.com>
2021-04-05 14:53:15 -0700

    feat: update keybindings to match upstreamed docs
    These keybindings (pulled from old documentation) were updated when
    improved LSP support was landed. This commit updates keybindings to use
    the bindings recommended by the neovim 0.5x docs.

--- lua/ndhoule_lsp.lua
+++ lua/ndhoule_lsp.lua
@@ -63,11 +63,13 @@ vim.lsp.handlers['textDocument/publishDiagnostics'] = handler
 --
 
 vim.cmd([[
-  nnoremap <silent> gd    <cmd>lua vim.lsp.buf.declaration()<CR>
-  nnoremap <silent> <c-]> <cmd>lua vim.lsp.buf.definition()<CR>
-  nnoremap <silent> K     <cmd>lua vim.lsp.buf.hover()<CR>
-  nnoremap <silent> gD    <cmd>lua vim.lsp.buf.implementation()<CR>
-  nnoremap <silent> <c-k> <cmd>lua vim.lsp.buf.signature_help()<CR>
-  nnoremap <silent> 1gD   <cmd>lua vim.lsp.buf.type_definition()<CR>
-  nnoremap <silent> gr    <cmd>lua vim.lsp.buf.references()<CR>
+  nnoremap <silent> <C-k>     <cmd>lua vim.lsp.buf.signature_help()<CR>
+  nnoremap <silent> <space>D  <cmd>lua vim.lsp.buf.type_definition()<CR>
+  nnoremap <silent> <space>q  <cmd>lua vim.lsp.diagnostic.set_loclist()<CR>
+  nnoremap <silent> <space>rn <cmd>lua vim.lsp.buf.rename()<CR>
+  nnoremap <silent> K         <cmd>lua vim.lsp.buf.hover()<CR>
+  nnoremap <silent> gD        <cmd>lua vim.lsp.buf.declaration()<CR>
+  nnoremap <silent> gd        <cmd>lua vim.lsp.buf.definition()<CR>
+  nnoremap <silent> gi        <cmd>lua vim.lsp.buf.implementation()<CR>
+  nnoremap <silent> gr        <cmd>lua vim.lsp.buf.references()<CR>
 ]])
