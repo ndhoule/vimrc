f691c6e264224b18d7f77ba767d5d6d94bb1c2bd
Nathan Houle <nathan@nathanhoule.com>
2021-08-28 22:42:42 -0700

    perf: disable unused built-in vim plugins

--- lua/ndhoule_general.lua
+++ lua/ndhoule_general.lua
@@ -10,6 +10,30 @@ vim.g.ale_disable_lsp = 1
 -- General Configuration
 --
 
+-- Disable built-in plugins
+
+vim.g.loaded_gzip = 1
+vim.g.loaded_zip = 1
+vim.g.loaded_zipPlugin = 1
+vim.g.loaded_tar = 1
+vim.g.loaded_tarPlugin = 1
+
+vim.g.loaded_getscript = 1
+vim.g.loaded_getscriptPlugin = 1
+vim.g.loaded_vimball = 1
+vim.g.loaded_vimballPlugin = 1
+vim.g.loaded_2html_plugin = 1
+
+-- vim.g.loaded_matchit = 1
+-- vim.g.loaded_matchparen = 1
+vim.g.loaded_logiPat = 1
+vim.g.loaded_rrhelper = 1
+
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+vim.g.loaded_netrwSettings = 1
+vim.g.loaded_netrwFileHandlers = 1
+
 vim.o.modelineexpr = true  -- Allow expressions in modelines
 
 -- Enable backup files
