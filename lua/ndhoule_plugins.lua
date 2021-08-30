987cfa312621267c87b13b6121805c8cc944a227
Nathan Houle <nathan@nathanhoule.com>
2021-08-30 00:42:22 -0700

    refactor: relocate LSP config to packer#use#config

--- lua/ndhoule_plugins.lua
+++ lua/ndhoule_plugins.lua
@@ -345,6 +345,38 @@ return require("packer").startup({
     use {
       'neovim/nvim-lspconfig',
       commit = '0eccc1a0ebf909aecfa1ac238d940061162ae84f',
+      config = function()
+        ---------------------------
+        -- General Configuration --
+        ---------------------------
+
+        local lspconfig = require('lspconfig')
+
+        lspconfig.bashls.setup{}
+        lspconfig.cssls.setup{}
+        lspconfig.dockerls.setup{}
+        lspconfig.html.setup{}
+        lspconfig.sqlls.setup{}
+        lspconfig.sumneko_lua.setup{cmd = {'lua-language-server'}}
+        lspconfig.terraformls.setup{}
+        lspconfig.tsserver.setup{}
+        lspconfig.vimls.setup{}
+        lspconfig.yamlls.setup{}
+
+        -----------------
+        -- Keybindings --
+        -----------------
+
+        vim.api.nvim_set_keymap('n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', { noremap = true, silent = true })
+        vim.api.nvim_set_keymap('n', '<space>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>', { noremap = true, silent = true })
+        vim.api.nvim_set_keymap('n', '<space>q', '<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>', { noremap = true, silent = true })
+        vim.api.nvim_set_keymap('n', '<space>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', { noremap = true, silent = true })
+        vim.api.nvim_set_keymap('n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', { noremap = true, silent = true })
+        vim.api.nvim_set_keymap('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>', { noremap = true, silent = true })
+        vim.api.nvim_set_keymap('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', { noremap = true, silent = true })
+        vim.api.nvim_set_keymap('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', { noremap = true, silent = true })
+        vim.api.nvim_set_keymap('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', { noremap = true, silent = true })
+      end
     }
 
     -- ## Syntax highlighting
