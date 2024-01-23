d64b38cde86d419d39d86ff316a9966d6159122c
Nathan Houle <nathan@nathanhoule.com>
2024-01-23 09:52:30 -0800

    perf: increase filesize limit before disabling features

--- lua/ndhoule/options.lua
+++ lua/ndhoule/options.lua
@@ -1,7 +1,7 @@
 -- Limits for large files used to disable functionality
 vim.g.filesize_limits = {
   lines = 10000,
-  size = 1024 * 8,
+  size = 1024 * 128,
 }
 
 vim.g.markdown_fenced_languages = {
